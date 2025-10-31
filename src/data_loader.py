1cb4035047b43741c1a68b436d343d95035c18bb
ferroblesmdq12 <fernandomdq12@gmail.com>
2025-10-30 22:42:32 -0300

    Organizando la arquitectura del proyecto - creando archivo data_loader.py de nuevo

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,5 @@
 # ===========================================
 # data_loader.py
-# Módulo para carga de datos desde Neon.tech
-# Autor: Fernando Raúl Robles
 # ===========================================
 
 import pandas as pd
@@ -11,12 +9,11 @@ import streamlit as st
 # FUNCIÓN DE CARGA DE DATOS
 # =============================
 
-@st.cache_data
+@st.cache_data(hash_funcs={object: lambda _: None})
 def load_data(conn):
     """
     Carga todas las tablas desde la base partnersdb (Neon.tech)
     y las devuelve como DataFrames de pandas.
-    Usa cache_data para mejorar rendimiento en producción.
     """
     try:
         partners = pd.read_sql("SELECT * FROM partners;", conn)
